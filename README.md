9a7ff251616e144e105b007b5ff8f71801f962fc
Luis Miguel Alvarado <lmar11xd@gmail.com>
2022-09-28 11:59:30 -0500

    tema 03

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Curso
+https://campus.open-bootcamp.com/cursos/1
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/components/container/task_list.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { LEVELS } from '../../models/levels.enum'
+import { Task } from '../../models/task.class'
+import TaskComponent from '../../pure/task';
+
+function TaskListComponent() {
+    const defaultTask = new Task('Example', 'Default description', false, LEVELS.NORMAL);
+
+    const changeState = (id) => {
+        console.log('Cambiar estado de una tarea');    
+    }
+
+    return (
+        <div>
+            <div>
+                <h1>Your Tasks:</h1> 
+            </div>
+            <TaskComponent task={defaultTask}></TaskComponent>
+        </div>
+    )
+}
+
+export default TaskListComponent
